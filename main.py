ba7a249a95031c5aaf02f1c45340e53b604424d7
Phindoleth <87788701+Phindoleth@users.noreply.github.com>
2023-03-23 09:19:27 -0400

    removed welcome window

--- main.py
+++ main.py
@@ -13,30 +13,8 @@ name = "FractureX"
 width = 900
 height = 800
 
-
-class WelcomeWindow(QDialog):
- def __init__(self, parent=None):
-     super().__init__(parent)
-     self.setWindowTitle("Welcome")
-     self.setFixedSize(QSize(400, 200))
-     dialogButton = QDialogButtonBox.StandardButton.Ok
-     self.dialogButton = QDialogButtonBox(dialogButton)
-     self.dialogButton.accepted.connect(self.accept)
-     self.dialogButton.rejected.connect(self.reject)
-     self.layout = QVBoxLayout()
-     label = QLabel("Welcome to FractureX!")
-     label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-     self.layout.addWidget(label)
-     self.layout.addWidget(self.dialogButton)
-     self.setLayout(self.layout)
-
-
-
-
 class MainWindow(QMainWindow):
  def __init__(self):
-     dialog = WelcomeWindow()
-     dialog.exec()
      super(MainWindow, self).__init__()
 
 
